cd289a0aaa5cca81eddfbb193b209b2f5e249b22
bvl <bennyspammy@gmail.com>
2020-03-01 21:37:38 +0100

    added auth context

--- src/App.js
+++ src/App.js
@@ -9,23 +9,27 @@ import Board from './component/Board';
 import Login from './component/pages/Login';
 import Register from './component/pages/Register';
 
+import { AuthProvider } from './context/Auth';
+
 function App() {
 	return (
-		<div className="App bg-light">
-			<Header />
-			<div className="container-fluid">
-				<div className="row">
-					<Sidebar />
-					<main className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
-						<Router>
-							<Route exact path="/" component={Board} />
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/register" component={Register} />
-						</Router>
-					</main>
+		<AuthProvider>
+			<div className="App bg-light">
+				<Header />
+				<div className="container-fluid">
+					<div className="row">
+						<Sidebar />
+						<main className="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+							<Router>
+								<Route exact path="/" component={Board} />
+								<Route exact path="/login" component={Login} />
+								<Route exact path="/register" component={Register} />
+							</Router>
+						</main>
+					</div>
 				</div>
 			</div>
-		</div>
+		</AuthProvider>
 	);
 }
 
