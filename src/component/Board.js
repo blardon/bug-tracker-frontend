6706c8e70f86b26d4b07e6cc1aaeab9a0bce2191
blardon <bennetvlardon@gmail.com>
2020-03-16 12:41:42 +0100

    added sprint selection and description to Issue Board

--- src/component/Board.js
+++ src/component/Board.js
@@ -1,4 +1,5 @@
 import React, { useState, useContext, useEffect } from 'react';
+import { Link } from 'react-router-dom';
 import { useQuery, useMutation } from '@apollo/react-hooks';
 import { DragDropContext } from 'react-beautiful-dnd';
 import uuid from 'uuid/v4';
@@ -51,6 +52,8 @@ function Board(props) {
 	const projectId = props.match.params.projectId;
 	const { user } = useContext(AuthContext);
 	const [ categories, setCategories ] = useState({});
+	const [ sprints, setSprints ] = useState([]);
+	const [ selectedSprint, setSelectedSprint ] = useState({});
 
 	const { data: project, loading } = useQuery(GET_PROJECT_BY_ID, { variables: { id: projectId } });
 
@@ -66,6 +69,10 @@ function Board(props) {
 					};
 				});
 				setCategories(categoriesFromBackend);
+				if (project.project.sprints) {
+					setSprints(project.project.sprints);
+					setSelectedSprint(project.project.sprints[0]);
+				}
 			}
 		},
 		[ project ]
@@ -77,16 +84,47 @@ function Board(props) {
 	if (!project || loading) {
 		return <div>Loading project...</div>;
 	}
-
 	return (
-		<div className="d-flex">
-			<DragDropContext onDragEnd={(result) => onDragEnd(result, categories, setCategories)}>
-				{Object.entries(categories).map(([ categoryId, category ], index) => {
-					return (
-						<Column key={categoryId} projectId={projectId} category={category} categoryId={categoryId} />
-					);
-				})}
-			</DragDropContext>
+		<div>
+			<div id="sprint_menu" className="row ml-2 mt-2 mr-2 mb-0 p-0 bg-white rounded box-shadow">
+				<ul class="nav nav-pills">
+					{sprints.map((spr) => {
+						return (
+							<li key={spr.id} class="nav-item">
+								<Link
+									onClick={() => setSelectedSprint(spr)}
+									className={spr.id === selectedSprint.id ? 'nav-link active' : 'nav-link'}
+									to="#"
+								>
+									{spr.title}
+								</Link>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+			<div id="sprint_desc" className="row ml-2 mt-2 mr-2 mb-0 p-0 bg-white rounded box-shadow">
+				<div className="col-12">
+					<h6 className="py-2 border-bottom border-gray pb-1">Description</h6>
+				</div>
+				<div className="col-12 mb-2">
+					<div className="">{selectedSprint.desc}</div>
+				</div>
+			</div>
+			<div className="d-flex">
+				<DragDropContext onDragEnd={(result) => onDragEnd(result, categories, setCategories)}>
+					{Object.entries(categories).map(([ categoryId, category ], index) => {
+						return (
+							<Column
+								key={categoryId}
+								projectId={projectId}
+								category={category}
+								categoryId={categoryId}
+							/>
+						);
+					})}
+				</DragDropContext>
+			</div>
 		</div>
 	);
 }
